2078e7bc0ebd570902f102a2be0ff7b09dbcc8b0
abdoul sarba <abdoulsarba@arolitec.loc>
2024-10-09 15:53:05 +0000

    ajout de nouveautruc

---
+++ script.ts
@@ -0,0 +1,31 @@
+// excerice 1
+let names:string
+let speed:number
+speed=25
+ let isLoading:boolean
+ let age:number|string
+
+ let  colors:string[]=[]
+ colors.push("red")
+ console.log(colors)
+ let infos:(string|boolean|number)[]=[];
+ infos.push("dupont",true,50)
+//  console.log(infos)
+ let members:object={
+    firstName:"Sarba",
+    Age:22,
+    IsLogging:false
+ }
+
+ console.log(members)
+
+
+ let number:{
+    FirstName:string,
+    Age:number,
+    IsLoading:boolean
+ }={
+    FirstName:"sarba abdoul",
+    Age:22,
+    IsLoading:false
+ }
