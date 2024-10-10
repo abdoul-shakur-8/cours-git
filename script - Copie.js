9becad84be9c91188b5991782939a5f06042f528
sarba <sarbaabdoul@gmail.com>
2024-10-10 10:10:37 +0000

    ajout en core

---
+++ script - Copie.js
@@ -0,0 +1,23 @@
+// excerice 1
+var names;
+var speed;
+speed = 25;
+var isLoading;
+var age;
+var colors = [];
+colors.push("red");
+console.log(colors);
+var infos = [];
+infos.push("dupont", true, 50);
+//  console.log(infos)
+var members = {
+    firstName: "Sarba",
+    Age: 22,
+    IsLogging: false
+};
+console.log(members);
+var number = {
+    FirstName: "sarba abdoul",
+    Age: 22,
+    IsLoading: false
+};
